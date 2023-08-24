4fd8db4dc25c3904136a2618b6e4c718e4e580f7
moskln <moussakhoualne5@gmail.com>
2023-08-24 16:07:13 -0700

    edite3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 import random
 
-# Generate a random number between -10000 and 10000
 number = random.randint(-10000, 10000)
 
-# Convert the number to a string
 string_number = str(number)
+last_digit = int(string_number[-1])
 
-# Get the absolute value of the last digit
-last_digit = abs(int(string_number[-1]))
+if number <= 0:
+    sign = "-"
+    print(f'Last digit of %s is {sign}%d' % (string_number, last_digit), end=" ")
 
-# Print the last digit of the number
-print('Last digit of %s is %d' % (string_number, last_digit), end=" ")
+else:
+    print('Last digit of %s is %d' % (string_number, last_digit), end=" ")
 
-# Check the properties of the last digit and print additional information
 if last_digit > 5:
     print('and is greater than 5')
 elif last_digit == 0:
     print('and is 0')
 else:
     print('and is less than 6 and not 0')
-
