cc1fc1bfc09d31b0b21c0beb9f6b95a89026cc80
moskln <moussakhoualne5@gmail.com>
2023-09-28 11:09:42 -0700

    imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
-add = __import__('add_0').add
+#!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
+result = add(a, b)
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, result))
+
